f495ab095839ca219877145783878e069423a479
RVEPRTY <luisofficialbusiness@gmail.com>
2026-06-01 22:07:14 -0500

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -9,7 +9,8 @@ const userSchema = new mongoose.Schema({
     lastRob: { type: Number, default: 0 },
     lastDaily: { type: Number, default: 0 },
     dailyStreak: { type: Number, default: 0 },
-    dmOptOut: { type: Boolean, default: false }
+    dmOptOut: { type: Boolean, default: false },
+    prestigeLevel: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('User', userSchema);

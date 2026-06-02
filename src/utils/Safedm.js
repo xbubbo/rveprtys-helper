885765cefbc64981803e886ac269a5c862407c03
RVEPRTY <luisofficialbusiness@gmail.com>
2026-06-01 21:59:26 -0500

    Check dmOptOut status before sending DM

--- src/utils/Safedm.js
+++ src/utils/Safedm.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 async function safeDM(client, userId, payload) {
     try {
-        const user = await User.findOne({ userId });
+        const user = await User.findOne({ userId, dmOptOut: true });
         if (user?.dmOptOut) return false;
         const discordUser = await client.users.fetch(userId);
         await discordUser.send(payload);
